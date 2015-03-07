a2b3aec97eb37f6d7681cd8e31d3f25b82137835
Anna <snowsigns67@gmail.com>
2015-03-07 14:39:24 -0600

    made changes

--- src/chatter/handler.clj
+++ src/chatter/handler.clj
@@ -2,20 +2,41 @@
   (:require [compojure.core :refer :all]
             [compojure.route :as route]
             [ring.middleware.defaults :refer [wrap-defaults site-defaults]]
-            [hiccup.page :as page]))
+            [hiccup.page :as page]
+            [hiccup.form :as form]
+            [ring.middleware.params :refer [wrap-params]]))
+
+(def chat-messages (atom '()))
 
 (defn generate-message-view
   "This generates the HTML for displaying messages"
-  []
+  [messages]
   (page/html5
-        [:head
-         [:title "chatter"]]
-        [:body
-         [:h1 "Our Chat App"]]))
+     [:head
+    [:title "chatter"]]
+   [:body
+    [:h1 "Our Chat App"]
+    [:p
+     (form/form-to
+     [:post "/"]
+     "Name: "(form/text-field "name")
+     "Message: "(form/text-field "msg")
+     (form/submit-button "Submit"))]
+     [:p
+     [:table
+      (map (fn [m] [:tr [:td (:name m)] [:td (:message m)]]) messages)]]]))
+
+(defn update-messages!
+  "This will update a message list atom"
+  [messages name new-message]
+  (swap! messages conj {:name name :message new-message}))
 
 (defroutes app-routes
-  (GET "/" [] (generate-message-view))
+  (GET "/" [] (generate-message-view @chat-messages))
+  (POST "/" {params :params} (generate-message-view
+                               (update-messages! chat-messages
+                                 (get params "name") (get params "msg"))
+                               ))
   (route/not-found "Not Found"))
 
-(def app
-  (wrap-defaults app-routes site-defaults))
+(def app (wrap-params app-routes))
