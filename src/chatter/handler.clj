756c6a69bdb21aa809d5a35f466f548f69ee2af5
Anna <snowsigns67@gmail.com>
2015-03-07 15:44:23 -0600

    preparing for heroku

--- src/chatter/handler.clj
+++ src/chatter/handler.clj
@@ -2,9 +2,13 @@
   (:require [compojure.core :refer :all]
             [compojure.route :as route]
             [ring.middleware.defaults :refer [wrap-defaults site-defaults]]
+            [ring.middleware.params :refer [wrap-params]]
+            [ring.adapter.jetty :as jetty]
             [hiccup.page :as page]
             [hiccup.form :as form]
-            [ring.middleware.params :refer [wrap-params]]))
+            [ring.util.anti-forgery :as anti-forgery]
+            [environ.core :refer [env]]))
+
 
 (def chat-messages (atom '()))
 
@@ -13,7 +17,10 @@
   [messages]
   (page/html5
      [:head
-    [:title "chatter"]]
+      [:title "chatter"]
+      (page/include-css "//maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap.min.css")
+      (page/include-js  "//maxcdn.bootstrapcdn.com/bootstrap/3.3.1/js/bootstrap.min.js")
+      (page/include-css "/chatter.css")]
    [:body
     [:h1 "Our Chat App"]
     [:p
@@ -23,7 +30,7 @@
      "Message: "(form/text-field "msg")
      (form/submit-button "Submit"))]
      [:p
-     [:table
+     [:table#messages.table.table-bordered.table-hover
       (map (fn [m] [:tr [:td (:name m)] [:td (:message m)]]) messages)]]]))
 
 (defn update-messages!
@@ -31,12 +38,27 @@
   [messages name new-message]
   (swap! messages conj {:name name :message new-message}))
 
+(defn init []
+  (println "chatter is starting"))
+
+
+(defn destroy []
+  (println "chatter is shutting down"))
+
+
 (defroutes app-routes
   (GET "/" [] (generate-message-view @chat-messages))
-  (POST "/" {params :params} (generate-message-view
-                               (update-messages! chat-messages
-                                 (get params "name") (get params "msg"))
-                               ))
+  (POST "/" {params :params}
+    (let [name-param (get params "name")
+          msg-param (get params "msg")
+          new-messages (update-messages! chat-messages name-param msg-param)]
+      (generate-message-view new-messages)
+      ))
+  (route/resources "/")
   (route/not-found "Not Found"))
 
 (def app (wrap-params app-routes))
+
+(defn -main [& [port]]
+  (let [port (Integer. (or port (env :port) 5000))]
+    (jetty/run-jetty #'app {:port port :join? false})))
